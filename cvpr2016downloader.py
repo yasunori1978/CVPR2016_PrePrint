49709af78a957f68b49f6ca2eccaedc03e7fc865
石井育規 <ishii@phi.local>
2016-05-05 13:55:07 +0900

    downloader commit third

--- cvpr2016downloader.py
+++ cvpr2016downloader.py
@@ -12,65 +12,59 @@ def ltitle_spliter( title ):
     title = title.split('<title>')[1].split('</title>')[0].rstrip().replace('&#8220','“').replace('&#8221','”').replace(':',' ').replace('<small>','').replace('</small>','').replace('<i>','').replace('</i>','').replace('<sup>','').replace('</sup>','').replace('-',' ').replace('&#8212',"—").replace('&#8216',"‘").replace('&#8217',"’").replace('&#239','ï').replace('&#246','ö').replace('&#126','~').replace('&#8211','–')
     return title
 
+
+
 for l in [ line  for line in res.readlines() if '<strong>' in line ]:
-    might_be_CVPR=0
+    pdf = ""
+    title = ""
+    aus = ""
+    abs = ""
+    
     if not '<a' in l:
         title = title_spliter(l)
+        url = 'http://export.arxiv.org/api/query?search_query=all:'+title+'&start=0&max_results=10'
+        pdf = ""
+        out = 0
+        authors = []
+        abses=[]
+        l_title = "abc"
         
-        url = 'http://export.arxiv.org/api/query?search_query=all:'+title+'&start=0&max_results=100'
-
-        try:
-            authors = []
-            pdf = ""
-            out = 0
-            abses=[]
-            u = urllib.urlopen(url)
-            for l in u.readlines():
-                if '<title>' in l:
-                    l_title = ltitle_spliter(l)
-                if '</arxiv:comment>' in l:
-                    if 'CVPR 2016' in l:
-                        might_be_CVPR = 1
-                if '<link title="pdf"' in l:
-                    pdf = l.split('"')[3]
-                if '<name>' in l:
-                    authors.append(l)
-                if '<summary>' in l or sum == 1:
-                    sum = 1
-                    abses.append(l)
-                if '</summary>' in l:
-                     sum = 0
-                aus = ""
-                abs = ""
-                for author in authors:
-                    aus += (author.split('<name>')[1].split('</name>')[0].rstrip())+","
-                for ab in abses:
-                    abs += ab.replace('<summary>',"").replace('</summary>',"").rstrip()
-            u.close()            
-        except IndexError:
-            pdf = "Not Found in ArXiv"
+        u = urllib.urlopen(url)
+        for l in u.readlines():
+            if '<title>' in l:
+                l_title = ltitle_spliter(l)
+            if '<link title="pdf"' in l:
+                pdf = l.split('"')[3]
+            if '<name>' in l:
+                authors.append(l)
+            if '<summary>' in l or sum == 1:
+                sum = 1
+                abses.append(l)
+            if '</summary>' in l:
+                sum = 0
             aus = ""
-            abs = ""
+            abs = ""                
 
-
-        if l_title.lower() in title.lower():
-            out = 1
-        else:
-            if might_be_CVPR == 0:        
-                pdf = "Not Found in ArXiv"
-                aus = ""
-                abs = ""
+            for author in authors:
+                aus += (author.split('<name>')[1].split('</name>')[0].rstrip())+","
+            for ab in abses:
+                abs += ab.replace('<summary>',"").replace('</summary>',"").rstrip()
+            if l_title.lower() in title.lower():
+                if aus != "" and abs != "" and pdf != "":
+                    out = 1 
+                    break
             else:
-                out = 1
+                authors = []
+                abses=[]
+        u.close()
+        
         if out == 0:
-                aus = ""
-                abs = ""
+            pdf = "Not Found in ArXiv"
+            authors = []
+            abses=[]
             
         print pdf+"\t"+title+"\t"+aus+"\t"+abs
-        if out == 1:
-            out = 0
-            break
-        out = 0
-
-        
+        aus = ""
+        abs = ""
+        pdf = ""
 
--- cvpr2016downloader_deep.py
+++ cvpr2016downloader_deep.py
@@ -3,122 +3,102 @@
 import urllib
 res = urllib.urlopen("http://cvpr2016.thecvf.com/program/main_conference")
 
+def title_spliter( title ):
+    title = l.split('<strong>')[1].split(';')[0].split('</strong>')[0].replace('&#8220','“').replace('&#8221','”').replace(':',' ').rstrip()
+    title = title.replace('<small>','').replace('</small>','').replace('<i>','').replace('</i>','').replace('<sup>','').replace('</sup>','').replace('-',' ').replace('&#8212',"—").replace('&#8216',"‘").replace('&#8217',"’").replace('&#239','ï').replace('&#246','ö').replace('&#126','~').replace('&#8211','–')    
+    return title
+
+def ltitle_spliter( title ):
+    title = title.split('<title>')[1].split('</title>')[0].rstrip().replace('&#8220','“').replace('&#8221','”').replace(':',' ').replace('<small>','').replace('</small>','').replace('<i>','').replace('</i>','').replace('<sup>','').replace('</sup>','').replace('-',' ').replace('&#8212',"—").replace('&#8216',"‘").replace('&#8217',"’").replace('&#239','ï').replace('&#246','ö').replace('&#126','~').replace('&#8211','–')
+    return title
+
+def deep_or_not ( l ):
+    deep = 0
+    if 'deep' in l:
+        deep = 1
+    elif 'Eeep' in l:
+        deep = 1
+    elif 'neural' in l:
+        deep = 1
+    elif 'Neural' in l:
+        deep = 1
+    elif 'convolutional' in l:
+        deep = 1
+    elif 'Convolutional' in l:
+        deep = 1
+    elif 'recurrent' in l:
+        deep = 1
+    elif 'Recurrent' in l:
+        deep = 1
+    elif 'LSTM' in l:
+        deep = 1
+    elif 'Long Short' in l:
+        deep = 1
+    elif 'network' in l:
+        deep = 1
+    elif 'Network' in l:
+        deep = 1
+    return deep
+
+
 for l in [ line  for line in res.readlines() if '<strong>' in line ]:
-    might_be_CVPR=0
+    pdf = ""
+    title = ""
+    aus = ""
+    abs = ""
+    
     if not '<a' in l:
-        title = l.split('<strong>')[1].split(';')[0].split('</strong>')[0].replace('&#8220','“').replace('&#8221','”').replace(':',' ').rstrip().replace('<small>','').replace('</small>','').replace('<i>','').replace('</i>','').replace('<sup>','').replace('</sup>','').replace('-',' ').replace('&#8212',"—").replace('&#8216',"‘").replace('&#8217',"’").replace('&#239','ï').replace('&#246','ö').replace('&#126','~').replace('&#8211','–')   
-
-        
-        url = 'http://export.arxiv.org/api/query?search_query=all:'+title+'&start=0&max_results=100'
-    u = urllib.urlopen(url)
-    try:
-
-        authors = []
+        title = title_spliter(l)
+        url = 'http://export.arxiv.org/api/query?search_query=all:'+title+'&start=0&max_results=10'
         pdf = ""
         out = 0
-        deep = 0
+        authors = []
         abses=[]
+        l_title = "abc"
         
-
+        u = urllib.urlopen(url)
         for l in u.readlines():
-
             if '<title>' in l:
-                l_title = l.split('<title>')[1].split('</title>')[0].rstrip().replace('&#8220','“').replace('&#8221','”').replace(':',' ').replace('<small>','').replace('</small>','').replace('<i>','').replace('</i>','').replace('<sup>','').replace('</sup>','').replace('-',' ').replace('&#8212',"—").replace('&#8216',"‘").replace('&#8217',"’").replace('&#239','ï').replace('&#246','ö').replace('&#126','~').replace('&#8211','–')  
-                if 'deep' in l:
-                    deep = 1
-                elif 'Eeep' in l:
-                    deep = 1
-                elif 'neural' in l:
-                    deep = 1
-                elif 'Neural' in l:
-                    deep = 1
-                elif 'convolutional' in l:
-                    deep = 1
-                elif 'Convolutional' in l:
-                    deep = 1
-                elif 'recurrent' in l:
-                    deep = 1
-                elif 'Recurrent' in l:
-                    deep = 1
-                elif 'LSTM' in l:
-                    deep = 1
-                elif 'Long Short' in l:
-                    deep = 1
-                elif 'network' in l:
-                    deep = 1
-                elif 'Network' in l:
-                    deep = 1
-
-
-                
-            if '</arxiv:comment>' in l:
-                if 'CVPR 2016' in l:
-                    might_be_CVPR = 1
-
-                    
+                l_title = ltitle_spliter(l)
             if '<link title="pdf"' in l:
                 pdf = l.split('"')[3]
             if '<name>' in l:
                 authors.append(l)
-            if '<summary>' in l or sum == 1  :
+            if '<summary>' in l or sum == 1:
                 sum = 1
-                if 'deep' in l:
-                    deep = 1
-                elif 'Eeep' in l:
-                    deep = 1
-                elif 'neural' in l:
-                    deep = 1
-                elif 'Neural' in l:
-                    deep = 1
-                elif 'convolutional' in l:
-                    deep = 1
-                elif 'Convolutional' in l:
-                    deep = 1
-                elif 'recurrent' in l:
-                    deep = 1
-                elif 'Recurrent' in l:
-                    deep = 1
-                elif 'LSTM' in l:
-                    deep = 1
-                elif 'Long Short' in l:
-                    deep = 1
-                elif 'network' in l:
-                    deep = 1
-                elif 'Network' in l:
-                    deep = 1
-
                 abses.append(l)
-
             if '</summary>' in l:
                 sum = 0
-                
-        aus = ""
-        abs = ""
-        for author in authors:
-            aus += (author.split('<name>')[1].split('</name>')[0].rstrip())+","
-        for ab in abses:
-            abs += ab.replace('<summary>',"").replace('</summary>',"").rstrip()
-    u.close()            
-    except IndexError:
-        pdf = "Not Found in ArXiv"
-        aus = ""
-        abs = ""
-
-
-    if l_title.lower() in title.lower():
-        out=1
-    else:
-        if might_be_CVPR == 0:        
-            pdf = "Not Found in ArXiv"
             aus = ""
-            abs = ""
-    if deep == 1:        
-        print pdf+"\t"+title+"\t"+aus+"\t"+abs
-        if out == 1:
-            out = 0
-            break
-    deep = 0
-    out = 0
-might_be_CVPR=0
+            abs = ""                
+
+            for author in authors:
+                aus += (author.split('<name>')[1].split('</name>')[0].rstrip())+","
+            for ab in abses:
+                abs += ab.replace('<summary>',"").replace('</summary>',"").rstrip()
+            if deep_or_not(title):
+                deep = 1
+                
+            if l_title.lower() in title.lower():
+                if aus != "" and abs != "" and pdf != "":
+                    if deep_or_not(l_title) or deep_or_not(abs) :
+                        deep = 1
+                        out = 1 
+                        break
+            else:
+                authors = []
+                abses=[]
+        u.close()
         
+        if out == 0:
+            pdf = "Not Found in ArXiv"
+            authors = []
+            abses=[]
+        if deep == 1:
+            print pdf+"\t"+title+"\t"+aus+"\t"+abs
+        out = 0    
+        deep = 0      
+        aus = ""
+        abs = ""
+        pdf = ""
 
